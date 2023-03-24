f0308665c9ffdf08a12ddccfc8b91fc32dac3360
MinSang406 <122081938+MinSang406@users.noreply.github.com>
2023-03-25 00:42:25 +0900

    [level 0] Title: 다항식 더하기, Time: 13.97 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,43 +1,17 @@
 class Solution {
     public String solution(String polynomial) {
-        String answer = "";
-        String[] arr = polynomial.split(" +");
-        int xNum = 0;
+        int xCount = 0;
         int num = 0;
 
-        for (String s : arr) {
-            if (s.equals("x")) {
-            	xNum += 1;
-            }
-            else if (s.contains("x")) {
-            	xNum += Integer.parseInt(s.substring(0, s.length() - 1));
-            }
-            else if (!s.equals("+")) {
+        for (String s : polynomial.split(" ")) {
+            if (s.contains("x")) {
+                xCount += s.equals("x") ? 1 : Integer.parseInt(s.replaceAll("x", ""));
+            } else if (!s.equals("+")) {
                 num += Integer.parseInt(s);
             }
         }
-
-        if (xNum != 0 && num == 0) {
-            if (xNum == 1) {
-                answer = "x";
-            }
-            else {
-                answer = xNum + "x";
-            }
-        } 
-
-        if (xNum != 0 && num != 0) {
-            if (xNum == 1) {
-                answer = "x" + " + " + num;
-            }
-            else {
-                answer = xNum + "x" + " + " + num;
-            }
-        }
-
-        if (xNum == 0 && num != 0) {
-            answer = String.valueOf(num);
-        }
-        return answer;
+        return (xCount != 0 ? xCount > 1 ? xCount + "x" : "x" : "") + 
+               (num != 0 ? (xCount != 0 ? " + " : "") + num : xCount == 0 ? "0" : "");
     }
 }
+
