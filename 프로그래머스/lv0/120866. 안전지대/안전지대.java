696f9486f10c272d689c88e467ebf241f45ea80e
MinSang406 <122081938+MinSang406@users.noreply.github.com>
2023-03-25 01:25:38 +0900

    [level 0] Title: 안전지대, Time: 0.69 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,31 +1,42 @@
 class Solution {
-     public int solution(int[][] board) {
-        int[][] boomBoard = new int[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1) checkExplosion(i, j, boomBoard);
+    public int solution(int[][] board) {
+        int answer = 0;
+        int length = board.length;   //길이
+        int[][] temp = new int[length+2][length+2];
+        // 길이를 2 늘린 액자용 배열 생성 -> 이러면 단순한 조건식으로 안전영역 구할 수 있음
+
+        // 액자에 board 이식.
+        for(int i=1; i<length+1; i++){
+            for(int j=1; j<length+1;j++){
+                temp[i][j]=board[i-1][j-1];
             }
         }
 
-        int count = 0;
-        for (int[] array : boomBoard) {
-            for (int value : array) {
-                if (value == 0) count++;
+
+
+        //위험지대 찾기
+        for(int i=1; i<length+1; i++){
+            for(int j=1; j<length+1;j++){
+                if(temp[i][j]==1){
+                    for(int a = i-1; a<=i+1; a++){
+                        for(int b =j-1; b<=j+1; b++){
+                            if(temp[a][b]!=1) temp[a][b]=2;
+                        }
+                    }
+                }
             }
         }
-        return count;
-    }
 
-    static void checkExplosion(int x, int y, int[][] boomBoard) {
-        int explosionX;
-        int explosionY;
-        int[] aroundX = {0, -1, -1, -1, 0, 0, 1, 1, 1};
-        int[] aroundY = {0, -1, 0, 1, -1, 1, -1, 0, 1};
-        for (int i = 0; i < 9; i++) {
-            explosionX = x + aroundX[i];
-            explosionY = y + aroundY[i];
-            if (explosionX < boomBoard.length && explosionY < boomBoard.length)
-                if (explosionX >= 0 && explosionY >= 0) boomBoard[explosionX][explosionY] = 1;
+        // 안전지대 카운트
+        for(int i=1; i<length+1; i++){
+            for(int j=1; j<length+1;j++){
+                if(temp[i][j]==0) answer++;
+                System.out.print(temp[i][j]);
+            }
+            System.out.println("");
         }
+
+
+        return answer;
     }
 }
