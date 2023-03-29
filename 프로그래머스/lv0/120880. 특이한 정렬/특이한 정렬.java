280ed384a82eceff746a6718ede4ca6e00bf9ee5
MinSang406 <122081938+MinSang406@users.noreply.github.com>
2023-03-30 01:52:04 +0900

    [level 0] Title: 특이한 정렬, Time: 0.02 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -2,12 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        List<Integer> nums = new ArrayList<>();
-		Arrays.sort(numlist);
-		for (int num : numlist) nums.add(num);
-		nums.sort((s1, s2) -> Integer.compare(Math.abs(s2 - n), Math.abs(s1 - n)));
-		Collections.reverse(nums);
-		return nums.stream().mapToInt(num -> num).toArray();
-	
+        int size = numlist.length;
+        for(int i=0; i<size-1; i++){
+            for(int k=i+1; k<size; k++){
+                int a = (numlist[i] - n) * (numlist[i] > n ? 1 : -1);
+                int b = (numlist[k] - n) * (numlist[k] > n ? 1 : -1);
+                if(a > b || (a == b && numlist[i] < numlist[k])){
+                    int temp = numlist[i];
+                    numlist[i] = numlist[k];
+                    numlist[k] = temp;
+                }
+
+            }
+        }
+        return numlist;
     }
 }
