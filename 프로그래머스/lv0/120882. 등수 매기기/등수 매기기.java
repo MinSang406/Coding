e7a9ca1747e1ffcde80c24d0d85664d9d39186d6
MinSang406 <122081938+MinSang406@users.noreply.github.com>
2023-03-30 22:10:17 +0900

    [level 0] Title: 등수 매기기, Time: 0.27 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+		List<Integer> scoreList = new ArrayList<>();
+		
+	        for(int[] i : score){
+	            scoreList.add(i[0] + i[1]);
+	        }
+	        
+	        scoreList.sort(Comparator.reverseOrder());
+
+	        for(int i = 0; i < score.length; i++){
+	            answer[i] = scoreList.indexOf(score[i][0] + score[i][1]) + 1;
+	        }
+        return answer;
+    }
+}
