24334927e0c0898ca52eff54ad3c0270fcad444e
MinSang406 <122081938+MinSang406@users.noreply.github.com>
2023-04-05 01:14:54 +0900

    [level 0] Title: k의 개수, Time: 1.95 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,19 +1,15 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-		String strK = String.valueOf(k);
-		
-		for(int x = i; x <= j; x++) {
-			String strX = String.valueOf(x);			
-			if(strX.contains(strK)) {
-				String[] xArr = strX.split("");			
-				for(int y = 0; y < xArr.length; y++) {
-					if(xArr[y].equals(strK)) {
-						answer++;
-					}
-				}
-			}
-		}
+
+        for (int num = i; num <= j; num++){
+            int tmp = num;
+            while (tmp != 0){
+                if (tmp % 10 == k)
+                    answer++;
+                tmp /= 10;
+            }
+        }
         return answer;
     }
 }
